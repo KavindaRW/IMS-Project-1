a87ed3bab63c798864ca71a40a58d71a823d1909
KavindaRW <116443764+KavindaRW@users.noreply.github.com>
2023-05-08 19:59:30 +0530

    IMS changes
    Uncle, I finished all the main UIs. But I need some help to develop the pop-ups and to design the interface of the Prize Giving since it should be automated.

--- IMS-Project/src/app/app.module.ts
+++ IMS-Project/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { AchievementsComponent } from './modulse/parent/components/achievements/achievements.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { ModulesComponent } from './admin/components/modules/modules.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AchievementsComponent
+    AchievementsComponent,
+    AdminComponent,
+    ModulesComponent
   ],
   imports: [
     BrowserModule,
